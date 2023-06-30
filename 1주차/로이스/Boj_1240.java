788b91e5bd703f373f35a2b9faac0906d9d92a69
Taeyeon <90550065+TaeyeonRoyce@users.noreply.github.com>
2023-06-30 09:05:49 +0900

    로건 피드백 반영

--- 1주차/로이스/Boj_1240.java
+++ 1주차/로이스/Boj_1240.java
@@ -16,18 +16,17 @@ public class Boj_1240 {
         final int N = Integer.parseInt(st.nextToken());
         final int M = Integer.parseInt(st.nextToken());
 
-        final List<Edge>[] nodes = new ArrayList[N + 1]; //이어진 node 번호 List를 가지는 node 번호의 배열
+        final List<Edge>[] nodes = new ArrayList[N + 1];
         for (int i = 1; i < N + 1; i++) {
             nodes[i] = new ArrayList<>();
         }
-      
         for (int i = 0; i < N - 1; i++) {
             final StringTokenizer nodeInfoSt = new StringTokenizer(br.readLine());
             final int from = Integer.parseInt(nodeInfoSt.nextToken());
             final int to = Integer.parseInt(nodeInfoSt.nextToken());
             final int weight = Integer.parseInt(nodeInfoSt.nextToken());
 
-            nodes[from].add(new Edge(to, weight)); //양방향 저장
+            nodes[from].add(new Edge(to, weight));
             nodes[to].add(new Edge(from, weight));
         }
 
@@ -39,29 +38,30 @@ public class Boj_1240 {
             final boolean[] visited = new boolean[N + 1];
             final Deque<int[]> queue = new ArrayDeque<>();
             queue.add(new int[]{from, 0});
+            visited[from] = true;
 
             while (!queue.isEmpty()) {
                 final int[] nodeWithDistance = queue.pollFirst();
                 final int nodeNumber = nodeWithDistance[0];
                 final int distanceFrom = nodeWithDistance[1];
-                if (visited[nodeNumber]) { //visited 탈출
-                    continue;
-                }
-                visited[nodeNumber] = true;
 
                 if (nodeNumber == to) {
                     System.out.println(distanceFrom);
                     break;
                 }
 
-              //BFS
-              //탐색중인 노드에 연결된 Edge들을 queue에 추가
                 final List<Edge> node = nodes[nodeNumber];
                 for (final Edge edge : node) {
+                    if (visited[edge.to]) {
+                        continue;
+                    }
+
                     queue.add(new int[]{edge.to, distanceFrom + edge.weight});
+                    visited[edge.to] = true;
                 }
             }
         }
+
     }
 
     private static class Edge {
